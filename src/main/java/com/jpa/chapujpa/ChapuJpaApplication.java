0b29e16d6699f420927de3933cee74e83c3ff013
Eddie Mallma Alvarez <emallma@proveedor.intercorp.com.pe>
2023-02-18 08:12:28 -0500

    first commit

---
+++ src/main/java/com/jpa/chapujpa/ChapuJpaApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.chapujpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChapuJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChapuJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpa/chapujpa/ChapuJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.chapujpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChapuJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
